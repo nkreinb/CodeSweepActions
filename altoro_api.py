66c7135ad2d958f84f73c25b08e397f78e86095b
nkreinb <39036024+nkreinb@users.noreply.github.com>
2025-05-20 12:36:02 -0400

    Update altoro_api.py

--- altoro_api.py
+++ altoro_api.py
@@ -91,8 +91,7 @@ def readCredsFromConfig(config_file):
     return (user,passwd)
 
 def file_exists(dir_path, file_name):
-    #vulnerability
-    p = subprocess.Popen(["dir", dir_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) 
+    p = subprocess.Popen(["dir", dir_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
     if(file_name in str(output)):
